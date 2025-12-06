1b51f935387dcb5f17356e65ca3d5a29f0cc3603
Shourya <shouryakesarwani573@gmail.com>
2025-12-06 23:29:04 +0530

    Scalar multiplication for a square matrix
    This program reads a square matrix, multiplies it by a scalar, and outputs the result.

--- DSA/Arrays/Multiplication by Scalar.cpp
+++ DSA/Arrays/Multiplication by Scalar.cpp
@@ -1,1 +1,32 @@
-
+#include <iostream>
+int main(){
+    int n;
+    std::cin>>n;
+    int arr1[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            std::cin>>arr1[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        std::cout<<arr1[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+    int scalar;
+    std::cin>>scalar;
+    int arr2[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr2[i][j]=scalar*arr1[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        std::cout<<arr2[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
