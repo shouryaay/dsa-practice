56ae6fb07508aa10ac5600683cc68f5c52b9e8d5
Shourya <shouryakesarwani573@gmail.com>
2025-12-17 23:44:06 +0530

    frequency counting for array elements
    program to calculate the frequency of elements in an array using unordered_map.

--- DSA/Hashing/countingfrequencies.cpp
+++ DSA/Hashing/countingfrequencies.cpp
@@ -1,1 +1,21 @@
-
+// calculating frequency of elements in an array
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    std::cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        std::cin>>arr[i];
+    }
+    // counts frequency
+    unordered_map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    // frequency of element
+    int f;
+    std::cin>>f;
+    std::cout<<mpp[f];
+    return 0;
+}
