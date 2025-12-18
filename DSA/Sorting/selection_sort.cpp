e489dcb23736631840e4b45896b4c1d936319e77
Shourya <shouryakesarwani573@gmail.com>
2025-12-18 21:46:40 +0530

    Selection sort

--- DSA/Sorting/selection_sort.cpp
+++ DSA/Sorting/selection_sort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 void selection_sort (vector<int>&arr){
     int n= arr.size();
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int min=i;
-        for(int j=i+1;j<n-2;j++){
+        for(int j=i+1;j<n;j++){
             if(arr[j]<arr[min]){
                 min=j;
             }
