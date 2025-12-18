f9b9770cf6d60d454c0c07cae19ee1a2ceef2f35
Shourya <shouryakesarwani573@gmail.com>
2025-12-19 02:40:36 +0530

    Insertion sort algorithm in c++

--- DSA/Sorting/insertion_sort.cpp
+++ DSA/Sorting/insertion_sort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 void insertion_sort(int arr[],int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int j=i;
         while(j>0 && arr[j-1]>arr[j]){
             swap(arr[j-1],arr[j]);
