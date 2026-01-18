2c9f593fb048498f2154fd3f9a57ebd03c7608fe
Shourya <shouryakesarwani573@gmail.com>
2026-01-18 18:46:37 +0530

    Longest consecutive sequence function

---
+++ DSA/MY_LEETCODE_PRBLMS/P128.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0){
+            return 0;
+        }
+        unordered_set <int> st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        int x;
+        int maxi=0;
+        int count = 0;
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                count = 1;
+                x=it;
+                while(st.find(x+1)!=st.end()){
+                    x=x+1;
+                    count++;
+                }
+            }
+            maxi=max(count,maxi);
+        }
+        return maxi;
+    }
+};
