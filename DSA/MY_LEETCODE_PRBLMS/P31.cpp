c3cd3539f0738eebd372876ca94ba7f893d7303c
Shourya <shouryakesarwani573@gmail.com>
2026-01-19 17:13:07 +0530

    NextPermutation function in P31.cpp
    Implement next permutation using C++ STL.

---
+++ DSA/MY_LEETCODE_PRBLMS/P31.cpp
@@ -0,0 +1,7 @@
+// using c++ stl (for its implementation refer to another P31 soln)
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
