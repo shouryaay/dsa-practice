438e491aa5f060e24b471357a4ba04c4c69b2073
Shourya <shouryakesarwani573@gmail.com>
2026-02-03 17:11:20 +0530

    Solution for finding largest subarray with sum 0

---
+++ DSA/Hashing/largesubtswith0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+    int n = arr.size();
+    if(n==0){
+        return 0;
+        }
+    unordered_map<int,int> mpp;
+    int sum=0;
+    int maxlen=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum==0){
+            maxlen=i+1;
+        }
+        else{
+            if(mpp.find(sum)!=mpp.end()){
+                maxlen=max(maxlen,i-mpp[sum]);
+            }
+            else{
+                mpp[sum]=i;
+            }
+        }
+    }
+    return maxlen;
+    }
+};
