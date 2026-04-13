f638595eecf1bbb073202bf383ed2d437f07c8d1
Shourya <shouryakesarwani573@gmail.com>
2026-04-13 14:45:23 +0530

    Add MinStack class with basic stack operations
    Implement MinStack class with push, pop, top, and getMin methods.

---
+++ DSA/MY_LEETCODE_PRBLMS/P155(BruteForce).cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    stack<pair<int,int>>stk;
+    void push(int val) {
+        if(stk.empty()){
+            stk.push({val,val});
+        }
+        else{
+            stk.push({val,min(stk.top().second,val)});
+        }
+    }
+    
+    void pop() {
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top().first;
+    }
+    
+    int getMin() {
+        return stk.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
