d13a48efaaf4c1b9441c04254e0dfcd9db6e7e7f
Shourya <shouryakesarwani573@gmail.com>
2026-09-04 22:08:37 +0530

    Time: 50 ms (1.37%) | Memory: 28.6 MB (10.45%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -6,27 +6,24 @@ public:
         int threem=0;
         int fivem=0;
         int sevenm=0;
-        unordered_map<int,int>mpp;
+        unordered_set<int>st;
         while(threem<=n || fivem<=n || sevenm<=n){
             threem=3*i;
             fivem=5*i;
             sevenm=7*i;
             if(threem<=n){
-                if(mpp.find(3*i)==mpp.end()){
+                if(st.insert(3*i).second){
                     sum+=3*i;
-                    mpp[3*i]=1;
                 }
             }
             if(fivem<=n){
-                if(mpp.find(5*i)==mpp.end()){
+                if(st.insert(5*i).second){
                     sum+=5*i;
-                    mpp[5*i]=1;
                 }
             }
             if(sevenm<=n){
-                if(mpp.find(7*i)==mpp.end()){
+                if(st.insert(7*i).second){
                     sum+=7*i;
-                    mpp[7*i]=1;
                 }
             }
             i++;
